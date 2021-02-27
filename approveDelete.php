a5ec2464bd48c33888a8b336731945ed3a4de83c
Makic <igormakic@gmail.com>
2021-02-27 15:54:17 +0100

    Changed approveDelete.php

--- approveDelete.php
+++ approveDelete.php
@@ -18,6 +18,82 @@ if(!$db->connect())exit();
     {
     ?>
     <h1>Approve or Delete</h1>
+    <div class="click">
+    <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="info.php">Info</a></li>
+    <li><a href="news.php">News</a></li>
+    <li><a href="profile.php">Profile</a></li>
+    <li><a href="users.php">Users</a></li>
+    <li><a href="post.php">Create a Topic</a></li>
+    <li><a href="approveDelete.php">Approve or Delete a Topic</a></li>
+    <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <hr>
+    <p>Selecet a news category you would like to see</p>
+    <form action="approveDelete.php" method="GET">
+    <select name="category" id="category">
+    <option value="0">Select a Category</option>
+    <?php 
+    $upit="SELECT * FROM categories";
+    $rez=$db->query($upit);
+    while($red=$db->fetch_object($rez)){
+    echo "<option value=$red->id>".$red->name."</option>";
+    }
+    ?>
+    </select><br><br>
+    <button>Select</button>
+    <?php
+        if(isset($_GET['category']))
+            {
+                if($_GET['category']!=0)
+                {
+                    $category=$_GET['category'];
+                    $upit="SELECT * FROM vnews WHERE deleted=1 and category='$category'";
+                    $rez=$db->query($upit);
+                    while($red=$db->fetch_object($rez))
+                    {
+                        echo "<div style='border: 1px solid black; width:300px'>";
+                        echo "<h3><a href='approveDelete.php?id=".$red->id."'>".$red->title."</a></h3>";
+                        echo "<p>".$red->text."</p>";
+                        $upit2="SELECT * FROM images WHERE newsid=$red->id";
+                        $rez2=$db->query($upit2);
+                        while($red2=$db->fetch_object($rez2))
+                            {
+                                echo "<img src={$red2->path} width='100' height='100'>";
+                            }
+                        echo "<p>Author: ".$red->username.". Created: <i>".$red->time."</i></p>";
+                        echo "</div>";
+                        echo "<p></p>";
+                    }
+
+                }
+                else echo "<p>You must select a category to view the news.</p>";
+            }
+            if(isset($_GET['id']))
+            {
+            if($_GET['id']!=0)
+                {
+                    $upit="SELECT * FROM vnews WHERE deleted=1 and id=".$_GET['id'];
+                    $rez=$db->query($upit);
+                            while($red=$db->fetch_object($rez))
+                            {
+                                echo "<div style='border: 1px solid black; width:400px'>";
+                                echo "<h3><a href='approveDelete.php?id=".$red->id."'>".$red->title."</a></h3>";
+                                echo "<p>".$red->text."</p>";
+                                $upit2="SELECT * FROM images WHERE newsid=$red->id";
+                                $rez2=$db->query($upit2);
+                                while($red2=$db->fetch_object($rez2))
+                                    {
+                                        echo "<img src={$red2->path} width='380' height='250'>";
+                                    }
+                                echo "<p>Author: ".$red->username.". Created: <i>".$red->time."</i></p>";
+                                echo "</div>";
+                            }
+                }
+            else echo "<p>Unable to find the given news id.<p>";
+            }
+    ?>
     <?php
     }
     else header('Location: index.php');
